21096b78b9dccd899f4a986a772bb5c7f3acf79a
Adam Greenspan <agreenspan24@gmail.com>
2017-07-10 20:27:44 +0300

    update

--- server.js
+++ server.js
@@ -19,36 +19,38 @@ var error = function (err, response, body) {
 var success = function (data) {
     console.log('Data [%s]', data);
 };
-{
-    "consumerKey": "XXX",
-    "consumerSecret": "XXX",
-    "accessToken": "XXX",
-    "accessTokenSecret": "XXX",
-    "callBackUrl": "XXX"
-}
-app.get('/', function (request, response) {
+var twitterDetails = {
+    "consumerKey": "0TzKzv9lrA54H1qqZzXwxDlOX",
+    "consumerSecret": "a48PJ04oiFPhmVPtxOwGfIIRxoLt15oP9EFH8JHk215YYXuD1z",
+    "accessToken": "970512554-iOdF04j6PnZnXSffqiaz0G9R5oIo099bFAFhXugA",
+    "accessTokenSecret": "zCEzwEtvGnK9jbcAUAIk36kNzCSV5IkhkL8RZmsxsK5Zf",
+    "callBackUrl": "https://vast-atoll-36697.herokuapp.com"
+};
+var twitter = new Twitter();
+
+
+twitter.getUserTimeline({ screen_name: 'teacher2teacher', count: '50'}, error, success);
+
+db.collection('statuses', function(error, coll) {
+    results.forEach(function(result){
+        coll.insert(result, function(error, saved) {
+            if (error) {
+                console.log("Error: " + error);
+                response.send(500);
+            }
+            else {
+                response.send();
+            }
+        });
+    })
+});
+app.get('/teacher2teacher', function (request, response) {
     response.set('Content-Type', 'text/html');
 
+    /*db.collection('statuses', function(error, coll) {
+        response.send(coll.find());
+    })*/
     //https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=teacher2teacher&count=50
-    var twitter = new Twitter();
-
-    //Example calls
-
-    twitter.getUserTimeline({ screen_name: 'teacher2teacher', count: '50'}, error, success);
-
-    db.collection('statuses', function(error, coll) {
-        results.forEach(function(result){
-            coll.insert(result, function(error, saved) {
-                if (error) {
-                    console.log("Error: " + error);
-                    response.send(500);
-                }
-                else {
-                    response.send('<html><head><title>Thanks!</title></head><body><h2>Thanks for your submission!</h2></body></html>');
-                }
-            });
-        })
-    });
 
     response.send('<p>Hey, it works!</p>');
 });
