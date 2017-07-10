5d469276eecc08715df6fb8b8a921f491c59dc69
Adam Greenspan <agreenspan24@gmail.com>
2017-07-10 23:59:22 +0300

    final Anne Lewis

---
+++ README.md
@@ -0,0 +1,11 @@
+Anne Lewis Coding Challenge:
+
+In order to run the following server, first please download the latest version of NodeJS, 
+    which can be done at https://nodejs.org/en/. 
+
+From there, move to the AnneLewis directory and run the following command "node server.js".
+
+After running that command, open the file "teacher2teacher.html" in your favorite browser. 
+
+That's it, enjoying seeing the latest tweets of @teacher2teacher, routed from twitter, saved on a local database, recalled by the server, sent over to a client, and displayed on your browser. Please let me know if you have any questions or run into any errors while reviewing!
+
--- server.js
+++ server.js
@@ -1,55 +1,47 @@
 // Express initialization
 var express = require('express');
 var app = express();
-var http = require('http');
+var bodyParser = require('body-parser');
 var Twitter = require('twitter-node-client').Twitter;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var mongoUri = 'mongodb://localhost:27017/AnneLewis';
 var MongoClient = require('mongodb').MongoClient;
-var mongoUri =  "mongodb://agreenspan24:Tchaikovsky's1812@testcluster-shard-00-00-jompl.mongodb.net:27017,testcluster-shard-00-01-jompl.mongodb.net:27017,testcluster-shard-00-02-jompl.mongodb.net:27017/?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin";
-var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
-    db = databaseConnection;
+var AnneLewis = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+    AnneLewis = databaseConnection;
 });
 
 var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
 };
 var success = function (data) {
-    console.log('Data [%s]', data);
+    var parsedData = JSON.parse(data);
+    parsedData.forEach(function(element) {
+        //console.log(element);
+        AnneLewis.collection('statuses').insertOne(element);
+    });
 };
-
 var twitter = new Twitter({
     "consumerKey": "0TzKzv9lrA54H1qqZzXwxDlOX",
-    "consumerSecret": "a48PJ04oiFPhmVPtxOwGfIIRxoLt15oP9EFH8JHk215YYXuD1z",
-    "accessToken": "970512554-iOdF04j6PnZnXSffqiaz0G9R5oIo099bFAFhXugA",
-    "accessTokenSecret": "zCEzwEtvGnK9jbcAUAIk36kNzCSV5IkhkL8RZmsxsK5Zf",
-    "callBackUrl": "https://vast-atoll-36697.herokuapp.com"
+    "consumerSecret": "a48PJ04oiFPhmVPtxOwGfIIRxoLt15oP9EFH8JHk215YYXuD1z"
 });
 
+twitter.getUserTimeline({screen_name: 'teacher2teacher', count: '10'}, error, success);
 
-twitter.getUserTimeline({ screen_name: 'teacher2teacher', count: '50'}, error, success);
-
-db.collection('statuses', function(error, coll) {
-    results.forEach(function(result){
-        coll.insert(result, function(error, saved) {
-            if (error) {
-                console.log("Error: " + error);
-                response.send(500);
-            }
-            else {
-                response.send();
-            }
-        });
-    })
-});
 app.get('/teacher2teacher', function (request, response) {
-    response.set('Content-Type', 'text/html');
+    response.set('Content-Type', 'application/json');
 
-    /*db.collection('statuses', function(error, coll) {
-        response.send(coll.find());
-    })*/
-    //https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=teacher2teacher&count=50
+    var statuses = AnneLewis.collection('statuses').find().toArray(function (error, data) {
+        response.send(data);
+    });
 
-    response.send('<p>Hey, it works!</p>');
 });
 
 app.listen(process.env.PORT || 3000);
