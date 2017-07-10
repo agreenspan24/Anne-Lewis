4c15b5f2c9221214068d1e4514f4f9ea66f2fee0
Adam Greenspan <agreenspan24@gmail.com>
2017-07-10 20:55:55 +0300

    updates

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ var app = express();
 var http = require('http');
 var Twitter = require('twitter-node-client').Twitter;
 
-mongodb://agreenspan24:Tchaikovsky's1812@testcluster-shard-00-00-jompl.mongodb.net:27017,testcluster-shard-00-01-jompl.mongodb.net:27017,testcluster-shard-00-02-jompl.mongodb.net:27017/?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin
-
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017';
-var MongoClient = require('mongodb').MongoClient, format = require('util').format;
-var ObjectId = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient;
+var mongoUri =  "mongodb://agreenspan24:Tchaikovsky's1812@testcluster-shard-00-00-jompl.mongodb.net:27017,testcluster-shard-00-01-jompl.mongodb.net:27017,testcluster-shard-00-02-jompl.mongodb.net:27017/?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin";
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
     db = databaseConnection;
 });
@@ -19,14 +16,14 @@ var error = function (err, response, body) {
 var success = function (data) {
     console.log('Data [%s]', data);
 };
-var twitterDetails = {
+
+var twitter = new Twitter({
     "consumerKey": "0TzKzv9lrA54H1qqZzXwxDlOX",
     "consumerSecret": "a48PJ04oiFPhmVPtxOwGfIIRxoLt15oP9EFH8JHk215YYXuD1z",
     "accessToken": "970512554-iOdF04j6PnZnXSffqiaz0G9R5oIo099bFAFhXugA",
     "accessTokenSecret": "zCEzwEtvGnK9jbcAUAIk36kNzCSV5IkhkL8RZmsxsK5Zf",
     "callBackUrl": "https://vast-atoll-36697.herokuapp.com"
-};
-var twitter = new Twitter();
+});
 
 
 twitter.getUserTimeline({ screen_name: 'teacher2teacher', count: '50'}, error, success);
